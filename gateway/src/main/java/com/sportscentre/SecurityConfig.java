8373b05e181b33b2c35ac7dbe89748c4cc7f7d6f
Freek van Lier <freek.vlier@gmail.com>
2022-11-30 15:32:39 +0100

    Working authentication

--- gateway/src/main/java/com/sportscentre/SecurityConfig.java
+++ gateway/src/main/java/com/sportscentre/SecurityConfig.java
@@ -1,23 +1,29 @@
 package com.sportscentre;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 
     @Override
-    protected void configure( HttpSecurity http ) throws Exception {
-
-        http.authorizeRequests()
-                .antMatchers( "/oauth2/**", "/login/**" ).permitAll()
-                .anyRequest().authenticated()
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and() // (1)
+                .authorizeRequests().anyRequest().authenticated() // (2)
                 .and()
-                .oauth2Login()
-                .defaultSuccessUrl( "/home" );
-
+                .oauth2ResourceServer().jwt(); // (3)
     }
 }
