48ecb1ceee0fe74dae668053ed6539ad1530b5ed
freekvlier <freek.vlier@gmail.com>
2022-12-21 20:08:36 +0100

    Kafka test

--- user-service/src/main/java/com/service/user/controller/UserController.java
+++ user-service/src/main/java/com/service/user/controller/UserController.java
@@ -19,8 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @RequestMapping("/test")
-    @GetMapping
+    @GetMapping("/test")
     @ResponseStatus(HttpStatus.OK)
     public String test(){
         return "test";
@@ -31,8 +30,8 @@ public class UserController {
         return userService.login(bearer);
     }
 
-    @DeleteMapping("/{userid}")
-    public boolean deleteUser(@RequestParam String userid) {
-        return userService.delete(userid);
+    @DeleteMapping("/delete")
+    public boolean deleteUser() {
+        return userService.delete();
     }
 }
